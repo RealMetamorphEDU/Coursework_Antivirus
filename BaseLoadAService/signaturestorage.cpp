3c270399476ddc92ec0368de519595dfcdb1ed78
RealMetamorph <andr.timchuk@yandex.ru>
2020-05-13 12:12:44 +0300

    Completed searching in record storage.

--- BaseLoadAService/signaturestorage.cpp
+++ BaseLoadAService/signaturestorage.cpp
@@ -2,6 +2,44 @@
 
 SignatureStorage::SignatureStorage(QObject *parent) : QObject(parent) {
     root = nullptr;
+    current = nullptr;
+    searching = false;
+}
+
+void SignatureStorage::startSearch() {
+    searching = true;
+    current = root;
+}
+
+QVector<SignatureRecord*> SignatureStorage::search(QByteArray data) {
+    QVector<SignatureRecord*> records;
+    if (searching) {
+        for (int i = 0; i < data.count();) {
+            switch (current->element == (byte) data.at(i) ? 0 : current->element > (byte) data.at(i) ? 1 : 2) {
+                case 0: // Equal
+                    if (current->isEnd)
+                        records.append(current->getRecords());
+                    current = current->equal;
+                    ++i;
+                    break;
+                case 1: // Left
+                    current = current->left;
+                    break;
+                case 2: // Right
+                    current = current->right;
+                    break;
+            }
+            if (current == nullptr) {
+                searching = false;
+                break;
+            }
+        }
+    }
+    return records;
+}
+
+bool SignatureStorage::isSearching() {
+    return searching;
 }
 
 SignatureStorage::~SignatureStorage() {
--- BaseLoadAService/signaturestorage.h
+++ BaseLoadAService/signaturestorage.h
@@ -9,10 +9,18 @@
 class SignatureStorage: public QObject {
 Q_OBJECT
     TSTNode *root;
+    bool searching;
+    TSTNode *current;
 public:
     explicit SignatureStorage(QObject *parent = nullptr);
+
     friend int AServiceBaseLoader::loadStorage(QString &storageName, QString &filepath);
     friend int AServiceBaseLoader::appendStorage(QString &storageName, QString &filepath) const;
+
+    void startSearch();
+    QVector<SignatureRecord*> search(QByteArray data);
+    bool isSearching();
+
     ~SignatureStorage();
 };
 
