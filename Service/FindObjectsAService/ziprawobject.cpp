6b67661e4743d6d05a9513abba619ef5a9895eec
RealMetamorph <andr.timchuk@yandex.ru>
2020-05-17 17:36:53 +0300

    Added ZipRawObject

--- Service/FindObjectsAService/ziprawobject.cpp
+++ Service/FindObjectsAService/ziprawobject.cpp
@@ -1,6 +1,50 @@
 #include "ziprawobject.h"
+#include "ZipArchive.h"
 
-ZipRawObject::ZipRawObject()
-{
+ZipRawObject::ZipRawObject(QString &filename, std::shared_ptr<ZipArchive> &archive,
+                           std::shared_ptr<ZipArchiveEntry> &entry,
+                           QObject *parent) : RawObject(parent) {
+    this->filename = filename;
+    this->archive = archive;
+    this->entry = entry;
+    this->source = nullptr;
+    this->source = this->entry->GetDecompressionStream();
+}
 
+QString ZipRawObject::getFullName() {
+    QString fullname;
+    fullname.append(filename).append("!");
+    fullname.append(QString::fromStdString(entry->GetFullName()));
+    return fullname;
+}
+
+qint64 ZipRawObject::getSize() {
+    return entry->GetSize();
+}
+
+QByteArray ZipRawObject::readBlock(qint64 offset, qint64 len) {
+    QByteArray data;
+    if (entry->CanExtract() && source != nullptr) {
+        source->seekg(offset);
+        char *buf = new char[len];
+        source->read(buf, len);
+        data.append(buf, source->gcount());
+        delete[] buf;
+    }
+    return data;
+}
+
+QByteArray ZipRawObject::readNextBlock(qint64 len) {
+    QByteArray data;
+    if (entry->CanExtract() && source != nullptr) {
+        char *buf = new char[len];
+        source->read(buf, len);
+        data.append(buf, source->gcount());
+        delete[] buf;
+    }
+    return data;
+}
+
+bool ZipRawObject::canRead() {
+    return entry->CanExtract() && source != nullptr;
 }
--- Service/FindObjectsAService/ziprawobject.h
+++ Service/FindObjectsAService/ziprawobject.h
@@ -1,11 +1,25 @@
 #ifndef ZIPRAWOBJECT_H
 #define ZIPRAWOBJECT_H
 
+#include "rawobject.h"
 
-class ZipRawObject
-{
+class ZipArchive;
+class ZipArchiveEntry;
+
+class ZipRawObject: public RawObject {
+    QString filename;
+    std::shared_ptr<ZipArchive> archive;
+    std::shared_ptr<ZipArchiveEntry> entry;
+    std::istream *source;
 public:
-    ZipRawObject();
+    ZipRawObject(QString& filename, std::shared_ptr<ZipArchive>& archive, std::shared_ptr<ZipArchiveEntry>& entry,
+                 QObject *parent = nullptr);
+
+    QString getFullName() override;
+    qint64 getSize() override;
+    QByteArray readBlock(qint64 offset, qint64 len) override;
+    QByteArray readNextBlock(qint64 len) override;
+    bool canRead() override;
 };
 
 #endif // ZIPRAWOBJECT_H
