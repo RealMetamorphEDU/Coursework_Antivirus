c2b33e3e0097c49afa70a181b751b21546fa0a22
RealMetamorph <andr.timchuk@yandex.ru>
2020-05-21 11:39:43 +0300

    Fixed ScanObject`s method signature (getRegion).

--- Service/FindObjectsAService/binaryscanobject.h
+++ Service/FindObjectsAService/binaryscanobject.h
@@ -15,7 +15,7 @@ public:
 
     QString getFullObjectName() override;
     int getRegionsCount() override;
-    const DataRegion& getRegion(int index) override;
+    const DataRegion getRegion(int index) override;
     qint64 getObjectSize() override;
     QByteArray readBlockFromRegion(int region, qint64 offset, qint64 len) override;
     QByteArray readNextBlockFromRegion(int region, qint64 len) override;
